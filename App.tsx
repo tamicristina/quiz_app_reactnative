27e0f0d09a6f0e916ebf9f9d7fcfb43ce9bef46a
tamicristina <tami.cristina06@gmail.com>
2022-09-05 21:31:30 -0300

    add theme colors

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import AppRoutes from "./src/routes/app.routes";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/styles/theme";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppRoutes />
+      <ThemeProvider theme={theme}>
+        <AppRoutes />
+      </ThemeProvider>
     </NavigationContainer>
   );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    primary: "#373737",
+    primary_ligth: "#959595",
+    secondary: "#FFFFFF",
+    sucess: "#52CE6D",
+    error: "#F24747",
+    purple_ligth: "#E0B0FF",
+    purple_dark: "#AD80CC",
+
+  },
+};
