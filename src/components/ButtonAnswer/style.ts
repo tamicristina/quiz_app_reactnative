cf85d8087ec2e27423746c690ead00ee5162ea7e
Tamires <79336444+tamicristina@users.noreply.github.com>
2022-11-29 08:34:01 -0300

    adjust styles

--- src/components/ButtonAnswer/style.ts
+++ src/components/ButtonAnswer/style.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components/native'
 
 
 export const ButtonContainer = styled.TouchableOpacity`
-  height: 50px;
+  height: 54px;
   background-color: ${({ theme }) => theme.colors.purple_ligth};
   justify-content: center;
   align-items: center;
-  margin: 10px 0;
+  margin: 12px 0;
   border-radius:5px;
 
   width:100%;
@@ -15,7 +15,7 @@ export const ButtonContainer = styled.TouchableOpacity`
 `
 
 export const ButtonLabel = styled.Text`
-  font-size: 24px;
+  font-size: 22px;
   color: ${({ theme }) => theme.colors.secondary};
 
 `
--- src/screens/Questions/style.ts
+++ src/screens/Questions/style.ts
@@ -4,7 +4,7 @@ export const Container = styled.View`
     flex: 1;
     align-items: center;
     justify-content: space-evenly;
-    padding: 0 15px;  
+    padding: 0 10px;  
     
 `
 
